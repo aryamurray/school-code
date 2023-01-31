230ea8c5179ec34ff45198b6f7c207e0813513b3
Inuayasha <themagician948@gmail.com>
2023-01-31 13:40:36 -0500

    salaar is next to me and approves this commit

---
+++ week4/loops.py
@@ -0,0 +1,31 @@
+# interations = 0
+# while interations < 10:
+#     print("hello lol")
+#     interations = interations + 1
+
+
+
+
+# iterations= int(150)
+
+# for i in range(iterations):
+#     print(i)
+
+
+
+# i=1
+# n=5
+# while i<=n:
+#     num=float(input("Enter number:"))
+#     if num > 0:
+#         print(num)
+#         i=+1
+#     else:
+#         print("erm, I can smell a problem going on....")
+
+
+length =4
+
+
+for i in range(length):
+    print(i+1)

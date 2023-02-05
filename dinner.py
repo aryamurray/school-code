f3887c94fb79bc34cd29930da1c1f222967ae9b5
Inuayasha <themagician948@gmail.com>
2023-02-05 13:55:49 -0500

    fixed dinner.py to reflect new assignment material

--- dinner.py
+++ dinner.py
@@ -44,125 +44,65 @@ for i in range(invitees):
     else:
         gf = False
 
-    print(keto,vegan,gf)
+    # Creating Space for Aesthetic
+    print("_______________________________________________________________\n")
+
     #Make reccomendations for food based on preferences !
     
     if keto == False and vegan == False and gf == False:
-        choice = input("Would you like 'Pizza', 'Pasta', 'Falafel', 'Steak' or just a 'Beverage'?:")
-        if choice == "Pizza":
-            cost = cost + 44.50
-            food_choices.append("Pizza")
-        elif choice == "Pasta":
-            cost = cost + 48.99
-            food_choices.append("Pasta")
-        elif choice == "Falafel":
-            cost = cost + 52.99
-            food_choices.append("Falafel")
-        elif choice == "Steak":
-            cost = cost + 49.60
-            food_choices.append("Steak")
-        elif choice == "Beverage":
-            cost = cost + 5.99
-            food_choices.append("Beverage")
-        else:
-            print("Invalid Input, Restarting.")
-            continue
+        print("We'll be serving you just a beverage then.")
+        cost = cost + 5.99
+        food_choices.append("Beverage")
     elif keto == False and vegan == False and gf == True:
-        choice = input("Would you like 'Falafel', 'Steak' or just a 'Beverage'?:")
-        if choice == "Falafel":
-            cost = cost + 52.99
-            food_choices.append("Falafel")
-        elif choice == "Steak":
-            cost = cost + 49.60
-            food_choices.append("Steak")
-        elif choice == "Beverage":
-            ccost = cost + 5.99
-            food_choices.append("Beverage")
+        print("We'll be serving you just a beverage then.")   
+        cost = cost + 5.99
+        food_choices.append("Beverage")
     elif keto == False and vegan == True and gf == False:
-        choice = input("Would you like 'Pizza', 'Pasta', 'Falafel', just a 'Beverage'?:")
-        if choice == "Pizza":
-            cost = cost + 44.50
-            food_choices.append("Pizza")
-        elif choice == "Pasta":
-            cost = cost + 48.99
-            food_choices.append("Pasta")
-        elif choice == "Falafel":
-            cost = cost + 52.99
-            food_choices.append("Falafel")
-        elif choice == "Beverage":
-            cost = cost + 5.99
-            food_choices.append("Beverage")
+        print("We'll be serving you a pasta then.") 
+        cost = cost + 48.99
+        food_choices.append("Pasta")
     elif keto == True and vegan == False and gf == False:
-        choice = input("Would you like 'Pizza', 'Falafel', 'Steak' or just a 'Beverage'?:")
-        if choice == "Pizza":
-            cost = cost + 44.50
-            food_choices.append("Pizza")
-        elif choice == "Falafel":
-            cost = cost + 52.99
-            food_choices.append("Falafel")
-        elif choice == "Steak":
-            cost = cost + 49.60
-            food_choices.append("Steak")
-        elif choice == "Beverage":
-            cost = cost + 5.99
-            food_choices.append("Beverage")
+        print("We'll be serving you just a beverage then.")   
+        cost = cost + 5.99
+        food_choices.append("Beverage")
     elif keto == True and vegan == True and gf == False:
-        choice = input("Would you like 'Pizza', 'Falafel', or just a 'Beverage'?:")
-        if choice == "Pizza":
-            cost = cost + 44.50
-            food_choices.append("Pizza")
-        elif choice == "Falafel":
-            cost = cost + 52.99
-            food_choices.append("Falafel")
-        elif choice == "Beverage":
-            cost = cost + 5.99
-            food_choices.append("Beverage")
+        print("We'll be serving you a Pizza then.")
+        cost = cost + 44.50
+        food_choices.append("Pizza")
     elif keto == False and vegan == True and gf == True:
-        choice = input("Would you like 'Falafel', or just a 'Beverage'?:")
-        if choice == "Falafel":
-            cost = cost + 52.99
-            food_choices.append("Falafel")
-        elif choice == "Beverage":
-            cost = cost + 5.99
-            food_choices.append("Beverage")
+        print("We'll be serving you just a beverage then.")
+        cost = cost + 5.99
+        food_choices.append("Beverage")
     elif keto == True and vegan == False and gf == True:
-        choice = input("Would you like 'Falafel', 'Steak' or just a 'Beverage'?:")
-        if choice == "Falafel":
-            cost = cost + 52.99
-            food_choices.append("Falafel")
-        elif choice == "Steak":
-            cost = cost + 49.60
-            food_choices.append("Steak")
-        elif choice == "Beverage":
-            cost = cost + 5.99
-            food_choices.append("Beverage")
+        print("We'll be serving you a Steak then")
+        cost = cost + 49.60
+        food_choices.append("Steak")
     elif keto == True and vegan == True and gf == True:
-        choice = input("Would you like 'Falafel', or just a 'Beverage'?:")
-        if choice == "Falafel":
-            cost = cost + 52.99
-            food_choices.append("Falafel")
-        elif choice == "Beverage":
-            cost = cost + 5.99
-            food_choices.append("Beverage")
-    else:
-        print("you've entered an invalid value, please restart the program")
-        continue
+        print("We'll be serving you a Falafel then")
+        cost = cost + 52.99
+        food_choices.append("Falafel")
+
+ # Creating Space for Aesthetic
+    print("_______________________________________________________________\n")
 
 
+
+    
 #Attain how much the user would like to tip
-
+tax = 1.13
 tip = ((int(input("How much do you want to tip your server (% percent)?:")) /100) + 1)
-post_tip_cost = (tip*cost)
+after_tax_cost = (cost*1.13)
+post_tip_cost = (tip*after_tax_cost)
 
 
 
 # Report to the user the orders that they have chosen
 print("You have X invitees with the following orders:")
-print(food_choices.count("Pizza"), 'invitees ordered Pizza. The cost is:$%.2f ' % (food_choices.count("Pizza")* 44.50 * tip), 2)
-print(food_choices.count("Pasta"), 'invitees ordered Pasta. The cost is:$%.2f ' % (food_choices.count("Pasta")* 48.99 * tip ))
-print(food_choices.count("Falafel"), 'invitees ordered Falafel. The cost is:$%.2f' % (food_choices.count("Falafel")* 52.99 * tip))
-print(food_choices.count("Steak"), 'invitees ordered Steak. The cost is:$%.2f ' % (food_choices.count("Steak")* 49.60 * tip))
-print(food_choices.count("Beverage"), 'invitees ordered only Beverage. The cost is:$%.2f' % (food_choices.count("Beverage")* 49.60 * tip) )
+print(food_choices.count("Pizza"), 'invitees ordered Pizza. The cost is:$%.2f ' % (food_choices.count("Pizza")* 44.50 * tax ), 2)
+print(food_choices.count("Pasta"), 'invitees ordered Pasta. The cost is:$%.2f ' % (food_choices.count("Pasta")* 48.99 * tax ))
+print(food_choices.count("Falafel"), 'invitees ordered Falafel. The cost is:$%.2f' % (food_choices.count("Falafel")* 52.99 * tax))
+print(food_choices.count("Steak"), 'invitees ordered Steak. The cost is:$%.2f ' % (food_choices.count("Steak")* 49.60 * tax))
+print(food_choices.count("Beverage"), 'invitees ordered only Beverage. The cost is:$%.2f' % (food_choices.count("Beverage")* 49.60 * tax) )
 
 # Creating Space for Aesthetic
 print("\n _______________________________________________________________\n")
@@ -171,5 +111,5 @@ print("\n _______________________________________________________________\n")
 final_cost = (post_tip_cost* 1.13)
 
 print("The total cost before tax is:$%.2f" % (cost))
-print("The total cost after tax is %.2f" % (cost*1.13))
-print("The total cost after %i%% is $%f" % ((tip-1)*100,final_cost))
+print("The total cost after tax is %.2f" % (cost*tax))
+print("The total cost after a %i%% tip is $%i" % ((tip-1)*100,final_cost))

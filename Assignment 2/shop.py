b91397cd9d595a8f4548bc4f577b62d9e6f37bb4
Inuayasha <themagician948@gmail.com>
2023-02-18 10:01:36 -0500

    started assignment 2

---
+++ Assignment 2/shop.py
@@ -0,0 +1,25 @@
+SSD = [['1', '250 GB', 69.99], ['2', '500 GB', 93.99], ['3', '4 TB', 219.99]]
+HDD = [['1', '500 GB', 106.33], ['2', '1 TB', 134.33]]
+CPU = [['1', 'Intel Core i7-11700K', 499.99], ['2', 'AMD Ryzen 7 5800X', 312.99]]
+MOTHERBOARD = [['1', 'MSI B550-A PRO', 197.46], ['2', 'MSI Z490-A PRO', 262.30]]
+RAM = [['1', '16 GB', 82.99], ['2', '32 GB', 174.99]]
+GRAPHICS_CARD = [['1', 'MSI GeForce RTX 3060 12GB', 539.99]]
+PSU = [['1', 'Corsair RM750', 164.99]]
+CASE = [['1', 'Full Tower (black)', 149.99], ['2', 'Full Tower (red)', 149.99]]
+
+PREBUILTS = [['1', 'Legion Tower Gen 7 with RTX 3080 Ti', 3699.99], ['2',
+'SkyTech Prism II Gaming PC', 2839.99], ['3', 'ASUS ROG Strix G10CE Gaming PC',
+1099.99]]
+
+
+def pickitems():
+    return 0
+
+def main():
+    print("Welcome to my PC Shop!!")
+    input("Would you like to build a custom PC? (1), purchase a prebuilt PC (2), or would you like to checkout? (3) :")
+
+
+
+
+main()

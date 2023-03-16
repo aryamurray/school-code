326c6caa373f5dc13be9d514e7366709a9e4d808
Inuayasha <themagician948@gmail.com>
2023-03-16 15:23:33 -0400

    assignment 3 finished no comments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os 
+import os
+
 
 def readPatientsFromFile(fileName):
     """
@@ -20,14 +21,14 @@ def readPatientsFromFile(fileName):
         ...
     }
     """
-  
+
     try:
         if os.path.isfile(fileName) == False:
-            raise ValueError("The file {} could not be found" .format(fileName))
+            raise ValueError(
+                "The file {} could not be found" .format(fileName))
     except ValueError as err:
         print("Error: {}" .format(err))
         exit()
-
 
     textline = 0
     patients = {}
@@ -38,7 +39,8 @@ def readPatientsFromFile(fileName):
             line = line.replace("\n", "")
             key, *values = line.split(",")
             if len(values) != 7:
-                print("Invalid number of fields ({}) in line: [{}]" .format(1 + len(values), textline))
+                print("Invalid number of fields ({}) in line: [{}]" .format(
+                    1 + len(values), textline))
                 continue
             else:
                 try:
@@ -56,25 +58,32 @@ def readPatientsFromFile(fileName):
                         values[values.index(index)] = int(index)
                 except ValueError:
                     print("Invalid data type in line: [{}]" .format(textline))
+                    continue
                 try:
                     if not 30 <= values[1] <= 43:
-                        raise ValueError("Invalid temperature value ({}) in line: [{}]" .format(values[1], textline))
+                        raise ValueError(
+                            "Invalid temperature value ({}) in line: [{}]" .format(values[1], textline))
                     if not 30 <= values[2] <= 200:
-                        raise ValueError("Invalid heart rate value ({}) in line: [{}]" .format(values[2], textline))
+                        raise ValueError(
+                            "Invalid heart rate value ({}) in line: [{}]" .format(values[2], textline))
                     if not 5 <= values[3] <= 60:
-                        raise ValueError("Invalid respiratory rate value ({}) in line: [{}]" .format(values[3], textline))
+                        raise ValueError("Invalid respiratory rate value ({}) in line: [{}]" .format(
+                            values[3], textline))
                     if not 50 <= values[4] <= 250:
-                        raise ValueError("Invalid systolic blood pressure value ({}) in line: [{}]" .format(values[4], textline))
+                        raise ValueError("Invalid systolic blood pressure value ({}) in line: [{}]" .format(
+                            values[4], textline))
                     if not 30 <= values[5] <= 150:
-                        raise ValueError("Invalid diastolic blood pressure value ({}) in line: [{}]" .format(values[5], textline))
+                        raise ValueError("Invalid diastolic blood pressure value ({}) in line: [{}]" .format(
+                            values[5], textline))
                     if not 80 <= values[6] <= 100:
-                        raise ValueError("Invalid oxygen saturation value ({}) in line: [{}]" .format(values[6], textline))
+                        raise ValueError("Invalid oxygen saturation value ({}) in line: [{}]" .format(
+                            values[6], textline))
                 except ValueError as err:
                     print(err)
+                    continue
                 patients.setdefault(key, []).append(values)
     contents.close()
     return patients
-
 
 
 def displayPatientData(patients, patientId=0):
@@ -95,21 +104,23 @@ def displayPatientData(patients, patientId=0):
             print("\n\nPatient ID: {}" .format(key))
             for sublist in patients[key]:
                 print("Visit Date: {}" .format(sublist[0]))
-                print("    Temperature: {}" .format(sublist[1]))
-                print("    Heart Rate: {}" .format(sublist[2]))
-                print("    Respitory Rate: {}" .format(sublist[3]))
-                print("    Systolic Blood Pressure: {}" .format(sublist[4]))
-                print("    Diastolic Blood Pressure: {}" .format(sublist[5]))
-                print("    Oxygen Saturation: {}" .format(sublist[6]))
-    elif str(patientId) in patients:
-        for sublist in patients[str(patientId)]:
+                print("    Temperature: {} C" .format(sublist[1]))
+                print("    Heart Rate: {} bpm" .format(sublist[2]))
+                print("    Respitory Rate: {} bpm" .format(sublist[3]))
+                print(
+                    "    Systolic Blood Pressure: {} mmHg" .format(sublist[4]))
+                print(
+                    "    Diastolic Blood Pressure: {} mmHg" .format(sublist[5]))
+                print("    Oxygen Saturation: {} %%" .format(sublist[6]))
+    elif patientId in patients:
+        for sublist in patients[patientId]:
             print("Visit Date: {}" .format(sublist[0]))
-            print(" Temperature: {}" .format(sublist[1]))
-            print("  Heart Rate: {}" .format(sublist[2]))
-            print("  Respitory Rate: {}" .format(sublist[3]))
-            print("  Systolic Blood Pressure: {}" .format(sublist[4]))
-            print("  Diastolic Blood Pressure: {}" .format(sublist[5]))
-            print("  Oxygen Saturation: {}" .format(sublist[6]))
+            print(" Temperature: {} C" .format(sublist[1]))
+            print("  Heart Rate: {} bpm" .format(sublist[2]))
+            print("  Respitory Rate: {} bpm" .format(sublist[3]))
+            print("  Systolic Blood Pressure: {} mmHg" .format(sublist[4]))
+            print("  Diastolic Blood Pressure: {} mmHg" .format(sublist[5]))
+            print("  Oxygen Saturation: {} %%" .format(sublist[6]))
     else:
         print("Patient with ID {} not found." .format(patientId))
 
@@ -122,10 +133,13 @@ def displayStats(patients, patientId=0):
     patientId: The ID of the patient to display vital signs for. If 0, vital signs will be displayed for all patients.
     """
     try:
-        if not isinstance(patientId, int):
-            raise TypeError("'patientId' should be an integer.")
-    except TypeError as err:
-        print("Error:", err)
+        patientId = int(patientId)
+    except ValueError:
+        print("Error: 'patiendId should be an integer.")
+        return
+    if type(patients) != dict:
+        print("Error: 'patients should be a dictionary.'")
+        return
     avgtemp = []
     avgheartrate = []
     avgresprate = []
@@ -154,14 +168,14 @@ def displayStats(patients, patientId=0):
         print(" Average Systolic Blood Presure: %.2f mmHg" % (avgsysbp))
         print(" Average Diastolic Blood Pressure: %.2f mmHg" % (avgdiabp))
         print(" Average Oxygen Saturation: %.2f %%" % (avgoxy))
-    elif str(patientId) in patients.keys():
+    elif patientId in patients.keys():
         for sublist in patients[patientId]:
-                avgtemp.append(float(sublist[1]))
-                avgheartrate.append(float(sublist[2]))
-                avgresprate.append(float(sublist[3]))
-                avgsysbp.append(float(sublist[4]))
-                avgdiabp.append(float(sublist[5]))
-                avgoxy.append(float(sublist[6]))
+            avgtemp.append(float(sublist[1]))
+            avgheartrate.append(float(sublist[2]))
+            avgresprate.append(float(sublist[3]))
+            avgsysbp.append(float(sublist[4]))
+            avgdiabp.append(float(sublist[5]))
+            avgoxy.append(float(sublist[6]))
         avgtemp = sum(avgtemp) / len(avgtemp)
         avgheartrate = sum(avgheartrate) / len(avgheartrate)
         avgresprate = sum(avgresprate) / len(avgresprate)
@@ -179,8 +193,6 @@ def displayStats(patients, patientId=0):
         print("No data found for patient with ID %i" % int(patientId))
 
 
-
-
 def addPatientData(patients, patientId, date, temp, hr, rr, sbp, dbp, spo2, fileName):
     """
     Adds new patient data to the patient list.
@@ -196,18 +208,43 @@ def addPatientData(patients, patientId, date, temp, hr, rr, sbp, dbp, spo2, file
     spo2: The patient's oxygen saturation level.
     fileName: The name of the file to append new data to.
     """
-    print("made it")
+    splitdate = date.split("-")
+    if len(splitdate[0]) != 4 or not 1 <= int(splitdate[1]) <= 12 or len(splitdate[1]) != 2 or not 1 <= int(splitdate[2]) <= 31 or len(splitdate[2]) != 2 or len(splitdate) > 3:
+        print("Invalid Date. Please enter a valid date.")
+        return
+    try:
+        if not 30 <= int(temp) <= 43:
+            raise ValueError(
+                "Invalid temperature. Please enter a temperature between 30 and 43 C")
+        if not 30 <= int(hr) <= 200:
+            raise ValueError(
+                "Invalid heart rate. Please enter a heart rate between 30 and 200 bpm.")
+        if not 5 <= int(rr) <= 60:
+            raise ValueError(
+                "Invalid respiratory rate. Please enter a respiratory rate between 5 and 60 bpm.")
+        if not 50 <= int(sbp) <= 250:
+            raise ValueError(
+                "Invalid systolic blood pressure. Please enter a systolic blood pressure between 50 and 250.")
+        if not 30 <= int(dbp) <= 150:
+            raise ValueError(
+                "Invalid diastolic blood pressure. Please enter a diastolic blood pressure between 30 and 150.")
+        if not 80 <= spo2 <= 100:
+            raise ValueError(
+                "Invalid oxygen saturation. Please enter an oxygen saturation between 80 and 100.")
+    except ValueError:
+        print(ValueError)
+
     with open(fileName, "a") as file:
         insert = f"\n{patientId},{date},{temp},{hr},{rr},{sbp},{dbp},{spo2}"
         file.write(insert)
     if patientId in patients.keys():
         oldpatients = patients[patientId]
-        oldpatients.append([patientId,date,temp,hr,rr,sbp,dbp,spo2])
+        oldpatients.append([patientId, date, temp, hr, rr, sbp, dbp, spo2])
         patients[patientId] = oldpatients
     else:
-        patients.update({patientId:[patientId,date,temp,hr,rr,sbp,dbp,spo2]})
+        patients.update(
+            {patientId: [patientId, date, temp, hr, rr, sbp, dbp, spo2]})
     print("Visit is saved successfully for patient #{}" .format(patientId))
-
 
 
 def findVisitsByDate(patients, year=None, month=None):
@@ -220,9 +257,27 @@ def findVisitsByDate(patients, year=None, month=None):
     return: A list of tuples containing patient ID and visit that match the filter.
     """
     visits = []
-    #######################
-    #### PUT YOUR CODE HERE
-    #######################
+    if month != None:
+        if not 1 <= month <= 12:
+            return visits
+
+    for patient in patients.keys():
+        for visit in patients[patient]:
+            dictdate = visit[0].split("-")
+            dictyear = int(dictdate[0])
+            dictmonth = int(dictdate[1])
+
+            if year == None:
+                visits.append((patient, visit))
+
+            elif month == None:
+                if year == dictyear:
+                    visits.append((patient, visit))
+
+            elif year == dictyear and month == dictmonth:
+                visits.append((patient, visit))
+            else:
+                continue
     return visits
 
 
@@ -234,9 +289,13 @@ def findPatientsWhoNeedFollowUp(patients):
     return: A list of patient IDs that need follow-up visits to to abnormal health stats.
     """
     followup_patients = []
-    #######################
-    #### PUT YOUR CODE HERE
-    #######################
+
+    for patient in patients.keys():
+        for visit in patients[patient]:
+            if not 60 < visit[2] < 100 or visit[4] > 140 or visit[5] >90 or visit[6] <90:
+                if patient not in followup_patients:
+                    followup_patients.append(int(patient))
+
     return followup_patients
 
 
@@ -249,11 +308,23 @@ def deleteAllVisitsOfPatient(patients, patientId, filename):
     filename: The name of the file to save the updated patient data.
     return: None
     """
-    #######################
-    #### PUT YOUR CODE HERE
-    #######################
+    if patientId not in patients.keys():
+        print("No data found for patient with ID {}".format(patientId))
+        return
+    
+    patients.pop(patientId)
 
+    with open(filename, "r") as contents:
+        lines = contents.readlines()
 
+    with open(filename, "w") as contents:
+        contents.truncate()
+        for line in lines:
+            strippedline = line.replace("\n", "")
+            indexline = strippedline.split(",")
+            if not int(indexline[0]) == patientId:
+                contents.write(line)
+    print("Data for patient {} has been deleted" .format(patientId))
 
 
 ###########################################################################
@@ -293,7 +364,8 @@ def main():
                 sbp = int(input("Enter systolic blood pressure (mmHg): "))
                 dbp = int(input("Enter diastolic blood pressure (mmHg): "))
                 spo2 = int(input("Enter oxygen saturation (%): "))
-                addPatientData(patients, patientID, date, temp, hr, rr, sbp, dbp, spo2, 'patients.txt')
+                addPatientData(patients, patientID, date, temp,
+                               hr, rr, sbp, dbp, spo2, 'patients.txt')
             except ValueError:
                 print("Invalid input. Please enter valid data.")
         elif choice == '4':
